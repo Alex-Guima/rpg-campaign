26b2bfda460439652e4a877b4aacebaa01e15a91
quarkusio <no-reply@quarkus.io>
2026-04-28 23:34:59 +0000

    Initial commit

---
+++ src/native-test/java/org/alex/guima/rpg/tracker/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.alex.guima.rpg.tracker;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
